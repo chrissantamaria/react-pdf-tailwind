2e012913c1b8bae47c5dc9523af5eb6da22a7935
Chris Santamaria <chris@santamaria.me>
2023-02-05 17:14:05 -0500

    Add dist/ to gitignore

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { TailwindTheme } from 'tailwindcss/tailwind-config';
-
-declare type StyleSheet = Record<string, string | number>;
-interface Config {
-    theme: TailwindTheme;
-}
-declare function createTw(config?: Config): (input: string) => StyleSheet;
-
-export { createTw as default };
